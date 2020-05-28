6cf864c5225dabb6e4ba9b25a585cf0e7317ffd4
Robin Siep <RobinSiep1@gmail.com>
2020-05-28 13:18:58 +0200

    mocked network dep

--- websocket_test.go
+++ websocket_test.go
@@ -9,17 +9,18 @@ import (
 )
 
 type ConnMock struct {
-    mock.mock
+    mock.Mock
 }
 
-func (mocked *ConnMock) Close() error {
+func (mocked ConnMock) Close() error {
     return nil
 }
 
-func (mocked *ConnMock) ReadMessage() (messageType int, p []byte, err error) {
+func (mocked ConnMock) ReadMessage() (messageType int, p []byte, err error) {
+    return 0, nil, nil
 }
 
-func (mocked *ConnMock) WriteMessage(messageType int, data []byte) error {
+func (mocked ConnMock) WriteMessage(messageType int, data []byte) error {
     return nil
 }
 
@@ -27,13 +28,15 @@ type DialerMock struct {
     mock.Mock
 }
 
-func (mocked *DialerMock) Dial(urlString string, header http.Header)(*WebsocketConn, *http.Response, error) {
-
+func (mocked DialerMock) Dial(urlString string, header http.Header)(WebsocketConn, *http.Response, error) {
+    return ConnMock{}, nil, nil
 }
 
 func TestCreateConnection(t *testing.T) {
     u := url.URL{Scheme: "ws", Host: "localhost:1970", Path: "/ws"}
-    connection, err := createConnection(u)
+    client := WebsocketClient{dialer: DialerMock{}}
+
+    connection, err := client.createConnection(u)
 
     if err != nil {
         t.Errorf("createConnection erred: %s", err)
