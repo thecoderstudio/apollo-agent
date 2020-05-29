aa125517aecb3f4447c501c09e5e4d906b74c181
Robin Siep <RobinSiep1@gmail.com>
2020-05-29 16:41:14 +0200

    spaces

--- client/client_test.go
+++ client/client_test.go
@@ -1,158 +1,158 @@
 package client_test
 
 import (
-	"errors"
-	"net/http"
-	"net/url"
-	"os"
-	"testing"
+    "errors"
+    "net/http"
+    "net/url"
+    "os"
+    "testing"
 
-	"github.com/gorilla/websocket"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/suite"
+    "github.com/gorilla/websocket"
+    "github.com/stretchr/testify/assert"
+    "github.com/stretchr/testify/mock"
+    "github.com/stretchr/testify/suite"
 
-	"github.com/thecoderstudio/apollo-agent/client"
+    "github.com/thecoderstudio/apollo-agent/client"
 )
 
 var u = url.URL{Scheme: "ws", Host: "localhost:8000", Path: "/ws"}
 
 type ConnMock struct {
-	mock.Mock
+    mock.Mock
 }
 
 func (mocked ConnMock) Close() error {
-	args := mocked.Called()
-	return args.Error(0)
+    args := mocked.Called()
+    return args.Error(0)
 }
 
 func (mocked ConnMock) ReadMessage() (messageType int, p []byte, err error) {
-	args := mocked.Called()
+    args := mocked.Called()
 
-	var message []byte
-	if args.Get(1) != nil {
-		message = args.Get(1).([]byte)
-	}
+    var message []byte
+    if args.Get(1) != nil {
+        message = args.Get(1).([]byte)
+    }
 
-	return args.Int(0), message, args.Error(2)
+    return args.Int(0), message, args.Error(2)
 }
 
 func (mocked ConnMock) WriteMessage(messageType int, data []byte) error {
-	args := mocked.Called(messageType, data)
-	return args.Error(0)
+    args := mocked.Called(messageType, data)
+    return args.Error(0)
 }
 
 type DialerMock struct {
-	mock.Mock
+    mock.Mock
 }
 
 func (mocked DialerMock) Dial(urlString string, header http.Header) (client.WebsocketConn, *http.Response, error) {
-	args := mocked.Called(urlString, header)
+    args := mocked.Called(urlString, header)
 
-	var connection client.WebsocketConn
-	if args.Get(0) != nil {
-		connection = args.Get(0).(client.WebsocketConn)
-	}
+    var connection client.WebsocketConn
+    if args.Get(0) != nil {
+        connection = args.Get(0).(client.WebsocketConn)
+    }
 
-	var response http.Response
-	if args.Get(1) != nil {
-		response = args.Get(1).(http.Response)
-	}
+    var response http.Response
+    if args.Get(1) != nil {
+        response = args.Get(1).(http.Response)
+    }
 
-	return connection, &response, args.Error(2)
+    return connection, &response, args.Error(2)
 }
 
 type ClientTestSuite struct {
-	suite.Suite
+    suite.Suite
 }
 
 func (suite *ClientTestSuite) TestListenSuccess() {
-	interrupt := make(chan os.Signal, 1)
+    interrupt := make(chan os.Signal, 1)
 
-	mockConn := new(ConnMock)
-	mockConn.On("Close").Return(nil)
-	mockConn.On(
-		"WriteMessage",
-		websocket.CloseMessage,
-		websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")).Return(nil)
-	mockConn.On("ReadMessage").Return(0, []byte("test message"), nil)
+    mockConn := new(ConnMock)
+    mockConn.On("Close").Return(nil)
+    mockConn.On(
+        "WriteMessage",
+        websocket.CloseMessage,
+        websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")).Return(nil)
+    mockConn.On("ReadMessage").Return(0, []byte("test message"), nil)
 
-	mockDialer := new(DialerMock)
-	mockDialer.On("Dial", u.String(), http.Header(nil)).Return(mockConn, nil, nil)
+    mockDialer := new(DialerMock)
+    mockDialer.On("Dial", u.String(), http.Header(nil)).Return(mockConn, nil, nil)
 
-	wsClient := client.Create(mockDialer)
-	out, done, _ := wsClient.Listen(u, &interrupt)
-	message := <-out
+    wsClient := client.Create(mockDialer)
+    out, done, _ := wsClient.Listen(u, &interrupt)
+    message := <-out
 
-	assert.Equal(suite.T(), message, "test message")
+    assert.Equal(suite.T(), message, "test message")
 
-	close(interrupt)
+    close(interrupt)
 
-	assert.NotNil(suite.T(), <-done)
+    assert.NotNil(suite.T(), <-done)
 }
 
 func (suite *ClientTestSuite) TestCloseConnectionWriteError() {
-	expectedError := errors.New("test")
-	interrupt := make(chan os.Signal, 1)
+    expectedError := errors.New("test")
+    interrupt := make(chan os.Signal, 1)
 
-	mockConn := new(ConnMock)
-	mockConn.On("Close").Return(nil)
-	mockConn.On(
-		"WriteMessage",
-		websocket.CloseMessage,
-		websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")).Return(expectedError)
-	mockConn.On("ReadMessage").Return(0, nil, nil)
+    mockConn := new(ConnMock)
+    mockConn.On("Close").Return(nil)
+    mockConn.On(
+        "WriteMessage",
+        websocket.CloseMessage,
+        websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")).Return(expectedError)
+    mockConn.On("ReadMessage").Return(0, nil, nil)
 
-	mockDialer := new(DialerMock)
-	mockDialer.On("Dial", u.String(), http.Header(nil)).Return(mockConn, nil, nil)
+    mockDialer := new(DialerMock)
+    mockDialer.On("Dial", u.String(), http.Header(nil)).Return(mockConn, nil, nil)
 
-	wsClient := client.Create(mockDialer)
-	out, done, _ := wsClient.Listen(u, &interrupt)
-	<-out
+    wsClient := client.Create(mockDialer)
+    out, done, _ := wsClient.Listen(u, &interrupt)
+    <-out
 
-	close(interrupt)
+    close(interrupt)
 
-	assert.NotNil(suite.T(), <-done)
-	mockConn.AssertExpectations(suite.T())
+    assert.NotNil(suite.T(), <-done)
+    mockConn.AssertExpectations(suite.T())
 }
 
 func (suite *ClientTestSuite) TestConnectionError() {
-	expectedError := errors.New("connection error")
-	interrupt := make(chan os.Signal, 1)
-	defer close(interrupt)
+    expectedError := errors.New("connection error")
+    interrupt := make(chan os.Signal, 1)
+    defer close(interrupt)
 
-	mockDialer := new(DialerMock)
-	mockDialer.On("Dial", u.String(), http.Header(nil)).Return(nil, nil, expectedError)
+    mockDialer := new(DialerMock)
+    mockDialer.On("Dial", u.String(), http.Header(nil)).Return(nil, nil, expectedError)
 
-	wsClient := client.Create(mockDialer)
-	_, _, errs := wsClient.Listen(u, &interrupt)
-	err := <-errs
+    wsClient := client.Create(mockDialer)
+    _, _, errs := wsClient.Listen(u, &interrupt)
+    err := <-errs
 
-	mockDialer.AssertExpectations(suite.T())
-	assert.EqualError(suite.T(), err, "connection error")
+    mockDialer.AssertExpectations(suite.T())
+    assert.EqualError(suite.T(), err, "connection error")
 }
 
 func (suite *ClientTestSuite) TestReadMessageError() {
-	expectedError := errors.New("read error")
-	interrupt := make(chan os.Signal, 1)
-	defer close(interrupt)
+    expectedError := errors.New("read error")
+    interrupt := make(chan os.Signal, 1)
+    defer close(interrupt)
 
-	mockConn := new(ConnMock)
-	mockConn.On("Close").Return(nil)
-	mockConn.On("ReadMessage").Return(0, nil, expectedError)
+    mockConn := new(ConnMock)
+    mockConn.On("Close").Return(nil)
+    mockConn.On("ReadMessage").Return(0, nil, expectedError)
 
-	mockDialer := new(DialerMock)
-	mockDialer.On("Dial", u.String(), http.Header(nil)).Return(mockConn, nil, nil)
+    mockDialer := new(DialerMock)
+    mockDialer.On("Dial", u.String(), http.Header(nil)).Return(mockConn, nil, nil)
 
-	wsClient := client.Create(mockDialer)
-	_, done, errs := wsClient.Listen(u, &interrupt)
-	err := <-errs
+    wsClient := client.Create(mockDialer)
+    _, done, errs := wsClient.Listen(u, &interrupt)
+    err := <-errs
 
-	assert.NotNil(suite.T(), <-done)
-	mockConn.AssertExpectations(suite.T())
-	assert.EqualError(suite.T(), err, "read error")
+    assert.NotNil(suite.T(), <-done)
+    mockConn.AssertExpectations(suite.T())
+    assert.EqualError(suite.T(), err, "read error")
 }
 
 func TestClientSuite(t *testing.T) {
-	suite.Run(t, new(ClientTestSuite))
+    suite.Run(t, new(ClientTestSuite))
 }
--- main.go
+++ main.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-	"flag"
-	"log"
-	"net/url"
-	"os"
-	"os/signal"
+    "flag"
+    "log"
+    "net/url"
+    "os"
+    "os/signal"
 
-	"github.com/thecoderstudio/apollo-agent/client"
+    "github.com/thecoderstudio/apollo-agent/client"
 )
 
 var addr = flag.String("addr", "", "host address")
 
 func main() {
-	flag.Parse()
-	log.SetFlags(0)
+    flag.Parse()
+    log.SetFlags(0)
 
-	if *addr == "" {
-		log.Fatal("--addr is required, please give a valid host address")
-	}
+    if *addr == "" {
+        log.Fatal("--addr is required, please give a valid host address")
+    }
 
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
+    interrupt := make(chan os.Signal, 1)
+    signal.Notify(interrupt, os.Interrupt)
 
-	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
-	wsClient := client.Create(new(client.DialWrapper))
-	out, done, errs := wsClient.Listen(u, &interrupt)
-	for {
-		select {
-		case msg := <-out:
-			log.Println(msg)
-		case err := <-errs:
-			log.Println(err)
-		case <-done:
-			return
-		}
-	}
+    u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+    wsClient := client.Create(new(client.DialWrapper))
+    out, done, errs := wsClient.Listen(u, &interrupt)
+    for {
+        select {
+        case msg := <-out:
+            log.Println(msg)
+        case err := <-errs:
+            log.Println(err)
+        case <-done:
+            return
+        }
+    }
 }
