16cf0e56ee03c197744c4d8fcfe1eae403247adf
Robin Siep <RobinSiep1@gmail.com>
2020-09-01 15:53:05 +0200

    inject mocks

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -1,9 +1,23 @@
 package api_test
 
 import (
+	"os"
 	"testing"
+
+	"github.com/thecoderstudio/apollo-agent/api"
+	"github.com/thecoderstudio/apollo-agent/mocks"
 )
 
 func TestMiddleware(t *testing.T) {
-	return
+	shellInterfaceMock := new(mocks.ShellInterface)
+	authProviderMock := new(mocks.AuthProvider)
+	interruptSignal := make(chan os.Signal, 1)
+	middleware := api.Middleware{
+		Host:            "localhost:8080",
+		InterruptSignal: &interruptSignal,
+		ShellInterface:  shellInterfaceMock,
+		OAuthClient:     authProviderMock,
+	}
+
+	middleware.Start("bash")
 }
