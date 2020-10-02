5d737fe25ad2596721176bd318a647894e044b3a
Robin Siep <RobinSiep1@gmail.com>
2020-10-02 16:21:44 +0200

    clean up code waiting for command completion

--- action/linpeas.go
+++ action/linpeas.go
@@ -1,11 +1,18 @@
 package action
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/thecoderstudio/apollo-agent/logging"
 	"github.com/thecoderstudio/apollo-agent/pty"
 )
+
+const initialisationIndication = "ADVISORY"
+const completionIndication = "linPEAS done"
+const commandFormat = "curl https://raw.githubusercontent.com/carlospolop/" +
+	"privilege-escalation-awesome-scripts-suite/master/linPEAS/" +
+	"linpeas.sh | sh && echo '%s\n'\n"
 
 // LinPeas allows for the execution of LinPEAS. LinPEAS is a script
 // to search for possible local privilege escalation paths
@@ -16,21 +23,31 @@ type LinPeas struct {
 
 // Run runs LinPEAS on the machine
 func (linPeas LinPeas) Run() {
-	go func() {
-		started := false
-		for {
-			output := <-linPeas.Session.Out()
-			if started && strings.Contains(output.Message, "linPEAS done") {
-				logging.Critical("DONEE")
-			} else if strings.Contains(output.Message, "ADVISORY") {
-				logging.Critical("true")
-				started = true
-			}
-		}
-	}()
+	go linPeas.waitForCompletion()
 	go linPeas.Session.Execute(
-		"curl https://raw.githubusercontent.com/carlospolop/" +
-			"privilege-escalation-awesome-scripts-suite/master/linPEAS/" +
-			"linpeas.sh | sh && echo 'linPEAS done\n'\n",
+		fmt.Sprintf(commandFormat, completionIndication),
 	)
+}
+
+func (linPeas LinPeas) waitForCompletion() {
+	linPeas.waitForInitialisation()
+	for {
+		if linPeas.outputContains(completionIndication) {
+			logging.Critical("DONEE")
+		}
+	}
+}
+
+func (linPeas LinPeas) waitForInitialisation() {
+	for {
+		if linPeas.outputContains(initialisationIndication) {
+			logging.Critical("init")
+			return
+		}
+	}
+}
+
+func (linPeas LinPeas) outputContains(substring string) bool {
+	output := <-linPeas.Session.Out()
+	return strings.Contains(output.Message, substring)
 }
