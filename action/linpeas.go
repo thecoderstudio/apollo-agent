9d8af13e46ff3f9fb96d8b1265825ef657522ded
Robin Siep <RobinSiep1@gmail.com>
2020-10-02 15:30:26 +0200

    rough done estimation code

--- action/linpeas.go
+++ action/linpeas.go
@@ -1,6 +1,9 @@
 package action
 
 import (
+	"strings"
+
+	"github.com/thecoderstudio/apollo-agent/logging"
 	"github.com/thecoderstudio/apollo-agent/pty"
 )
 
@@ -13,9 +16,21 @@ type LinPeas struct {
 
 // Run runs LinPEAS on the machine
 func (linPeas LinPeas) Run() {
+	go func() {
+		started := false
+		for {
+			output := <-linPeas.Session.Out()
+			if started && strings.Contains(output.Message, "linPEAS done") {
+				logging.Critical("DONEE")
+			} else if strings.Contains(output.Message, "ADVISORY") {
+				logging.Critical("true")
+				started = true
+			}
+		}
+	}()
 	go linPeas.Session.Execute(
 		"curl https://raw.githubusercontent.com/carlospolop/" +
 			"privilege-escalation-awesome-scripts-suite/master/linPEAS/" +
-			"linpeas.sh | sh\n",
+			"linpeas.sh | sh && echo 'linPEAS done\n'\n",
 	)
 }
